d4da7c07ab4280da2e6c3e846bf20546cbf3a9e6
priscillavassallo <priscillavassallo96@gmail.com>
2025-11-18 18:03:05 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,29 @@
+
+var jogador = null;
+var jogadorSelecionado = document.getElementById('jogador-selecionado');
+
+mudarJogador('X');
+
+function escolherQuadrado(id){
+    var quadrado = document.getElementById(id);
+    if (quadrado.innerHTML !== '-'){
+        return;
+    }
+
+    quadrado.innerHTML = jogador;
+    quadrado.style.color = '#000';
+
+    if (jogador === 'X'){
+        jogador = 'O';
+    } else {
+        jogador = 'X';
+    }
+
+    mudarJogador(jogador);
+
+}
+
+function mudarJogador(valor){
+    jogador = valor;
+    jogadorSelecionado.innerHTML = jogador;
+}
